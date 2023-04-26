0156d7c60417984cf0e85d5e170589415c34b261
Bingsu <ks2515@naver.com>
2023-04-26 21:38:15 +0900

    fix: version

--- adetailer/__init__.py
+++ adetailer/__init__.py
@@ -1,5 +1,12 @@
+from .__version__ import __version__
 from .common import PredictOutput, get_models
 from .mediapipe import mediapipe_predict
 from .ultralytics import ultralytics_predict
 
-__all__ = ["PredictOutput", "get_models", "mediapipe_predict", "ultralytics_predict"]
+__all__ = [
+    "__version__",
+    "PredictOutput",
+    "get_models",
+    "mediapipe_predict",
+    "ultralytics_predict",
+]
