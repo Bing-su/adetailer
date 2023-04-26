f52708d380c38cce86ac831e554303b35f55e1d3
Bingsu <ks2515@naver.com>
2023-04-26 16:41:23 +0900

    feat: add cv functions

--- adetailer/common.py
+++ adetailer/common.py
@@ -4,8 +4,10 @@ from collections import OrderedDict
 from pathlib import Path
 from typing import Optional
 
+import cv2
+import numpy as np
 from huggingface_hub import hf_hub_download
-from PIL import Image, ImageDraw
+from PIL import Image, ImageChops, ImageDraw
 from pydantic.dataclasses import dataclass
 
 
@@ -67,3 +69,66 @@ def create_mask_from_bbox(
         mask_draw.rectangle(bbox, fill=255)
         masks.append(mask)
     return masks
+
+
+def _dilate(arr: np.ndarray, value: int) -> np.ndarray:
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (value, value))
+    return cv2.dilate(arr, kernel, iterations=1)
+
+
+def _erode(arr: np.ndarray, value: int) -> np.ndarray:
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (value, value))
+    return cv2.erode(arr, kernel, iterations=1)
+
+
+def dilate_erode(img: Image.Image, value: int) -> Image.Image:
+    """
+    The dilate_erode function takes an image and a value.
+    If the value is positive, it dilates the image by that amount.
+    If the value is negative, it erodes the image by that amount.
+
+    Parameters
+    ----------
+        img: PIL.Image.Image
+            the image to be processed
+        value: int
+            kernel size of dilation or erosion
+
+    Returns
+    -------
+        PIL.Image.Image
+            The image that has been dilated or eroded
+    """
+    if value == 0:
+        return img
+
+    arr = np.array(img)
+    arr = _dilate(arr, value) if value > 0 else _erode(arr, -value)
+
+    return Image.fromarray(arr)
+
+
+def offset(img: Image.Image, x: int = 0, y: int = 0) -> Image.Image:
+    """
+    The offset function takes an image and offsets it by a given x(→) and y(↑) value.
+
+    Parameters
+    ----------
+        mask: Image.Image
+            Pass the mask image to the function
+        x: int
+            →
+        y: int
+            ↑
+
+    Returns
+    -------
+        PIL.Image.Image
+            A new image that is offset by x and y
+    """
+    return ImageChops.offset(img, x, -y)
+
+
+def is_all_black(img: Image.Image) -> bool:
+    arr = np.array(img)
+    return cv2.countNonZero(arr) == 0
