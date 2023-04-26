ff4f99ecf40d2f48c7071b51bbce2d86e1bd37cd
Bingsu <ks2515@naver.com>
2023-04-26 23:08:30 +0900

    fix: boxes attribute error

--- adetailer/ultralytics.py
+++ adetailer/ultralytics.py
@@ -18,7 +18,7 @@ def ultralytics_predict(
     model = YOLO(model_path)
     pred = model(image, conf=confidence, hide_labels=True)
 
-    bboxes = pred[0].xyxy.cpu().numpy()
+    bboxes = pred[0].boxes.xyxy.cpu().numpy()
     if bboxes.size == 0:
         return PredictOutput()
     bboxes = bboxes.tolist()
