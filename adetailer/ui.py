27591dfbe78928ab166029f12ac2fb01c15e02ad
Bingsu <ks2515@naver.com>
2023-09-02 16:26:02 +0900

    stype: apply pre-commit

--- adetailer/ui.py
+++ adetailer/ui.py
@@ -463,7 +463,6 @@ def inpainting(w: Widgets, n: int, is_img2img: bool, webui_info: WebuiInfo):
                     elem_id=eid("ad_vae"),
                 )
 
-
         with gr.Row(), gr.Column(variant="compact"):
             w.ad_use_sampler = gr.Checkbox(
                 label="Use separate sampler" + suffix(n),
