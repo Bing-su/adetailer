645c67e3ffaeba240c6f4d1e344d0660dff4e3e4
Bingsu <ks2515@naver.com>
2023-09-10 22:07:02 +0900

    fix: function -> list

--- adetailer/ui.py
+++ adetailer/ui.py
@@ -29,8 +29,8 @@ class WebuiInfo:
     sampler_names: list[str]
     t2i_button: gr.Button
     i2i_button: gr.Button
-    checkpoints_list: Callable[..., list[str]]
-    vae_list: Callable[..., list[str]]
+    checkpoints_list: list[str]
+    vae_list: list[str]
 
 
 def gr_interactive(value: bool = True):
@@ -427,11 +427,7 @@ def inpainting(w: Widgets, n: int, is_img2img: bool, webui_info: WebuiInfo):
                     elem_id=eid("ad_use_checkpoint"),
                 )
 
-                ckpts = ["Use same checkpoint"]
-                try:
-                    ckpts.extend(webui_info.checkpoints_list(use_short=True))
-                except TypeError:
-                    ckpts.extend(webui_info.checkpoints_list())
+                ckpts = ["Use same checkpoint", *webui_info.checkpoints_list]
 
                 w.ad_checkpoint = gr.Dropdown(
                     label="ADetailer checkpoint" + suffix(n),
@@ -449,7 +445,7 @@ def inpainting(w: Widgets, n: int, is_img2img: bool, webui_info: WebuiInfo):
                     elem_id=eid("ad_use_vae"),
                 )
 
-                vaes = ["Use same VAE", *webui_info.vae_list()]
+                vaes = ["Use same VAE", *webui_info.vae_list]
 
                 w.ad_vae = gr.Dropdown(
                     label="ADetailer VAE" + suffix(n),
--- scripts/!adetailer.py
+++ scripts/!adetailer.py
@@ -120,13 +120,20 @@ class AfterDetailerScript(scripts.Script):
         num_models = opts.data.get("ad_max_models", 2)
         ad_model_list = list(model_mapping.keys())
         sampler_names = [sampler.name for sampler in all_samplers]
+
+        try:
+            checkpoint_list = modules.sd_models.checkpoint_tiles(use_shorts=True)
+        except TypeError:
+            checkpoint_list = modules.sd_models.checkpoint_tiles()
+        vae_list = modules.shared_items.sd_vae_items()
+
         webui_info = WebuiInfo(
             ad_model_list=ad_model_list,
             sampler_names=sampler_names,
             t2i_button=txt2img_submit_button,
             i2i_button=img2img_submit_button,
-            checkpoints_list=modules.sd_models.checkpoint_tiles,
-            vae_list=modules.shared_items.sd_vae_items,
+            checkpoints_list=checkpoint_list,
+            vae_list=vae_list,
         )
 
         components, infotext_fields = adui(num_models, is_img2img, webui_info)
