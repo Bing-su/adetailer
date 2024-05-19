41189c607d9d4e98605a8470dff83e6b221d6541
Dowon <ks2515@naver.com>
2024-05-19 20:57:04 +0900

    fix: discontinued message

--- aaaaaa/conditional.py
+++ aaaaaa/conditional.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
 from PIL import Image
+from rich import print
 
 try:
     from modules.processing import create_binary_mask
 except ImportError:
+    msg = "[-] ADetailer: Support for webui versions below 1.6.0 will be discontinued."
+    print(msg)
 
     def create_binary_mask(image: Image.Image):
         return image.convert("L")
