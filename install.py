9f52c852c2b4a09110d498bd0b60f2f6192a1f7c
Dowon <ks2515@naver.com>
2024-06-13 20:00:34 +0900

    fix(install): protobuf==3 available

--- install.py
+++ install.py
@@ -47,10 +47,6 @@ def install():
         ("ultralytics", "8.2.0", None),
         ("mediapipe", "0.10.13", None),
         ("rich", "13.0.0", None),
-        # mediapipe
-        ("protobuf", "4.25.3", "4.9999"),
-        # protobuf
-        ("open-clip-torch", "2.23.0", "2.9999"),
     ]
 
     pkgs = []
