06100063b3c0886d9c0bc416f79f29a59b46417e
Dowon <ks2515@naver.com>
2025-02-18 20:18:42 +0900

    feat: change unsafe pickling

--- aaaaaa/helper.py
+++ aaaaaa/helper.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
+import os
 from contextlib import contextmanager
 from copy import copy
 from typing import TYPE_CHECKING, Any, Union
+from unittest.mock import patch
 
 import torch
 from PIL import Image
 from typing_extensions import Protocol
 
 from modules import safe
-from modules.shared import opts
+from modules.shared import cmd_opts, opts
 
 if TYPE_CHECKING:
     # 타입 체커가 빨간 줄을 긋지 않게 하는 편법
@@ -34,6 +36,15 @@ def change_torch_load():
         yield
     finally:
         torch.load = orig
+
+
+@contextmanager
+def disable_safe_unpickle():
+    with (
+        patch.dict(os.environ, {"TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD": "1"}, clear=False),
+        patch.object(cmd_opts, "disable_safe_unpickle", True),
+    ):
+        yield
 
 
 @contextmanager
